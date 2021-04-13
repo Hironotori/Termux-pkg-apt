41ca8eeb120a5f2fac1338c8622098d059c0b5a9
Hironotori <81866681+Hironotori@users.noreply.github.com>
2021-04-13 10:50:23 +0300

    Version 1.5

--- Termux-pkg-apt.py
+++ Termux-pkg-apt.py
@@ -19,44 +19,46 @@ def slowprint(s):
 
 slowprint(''' \033[93m
               Все что будет установлена
-[01] apt-pkg python
-[02] apt-pkg python2
-[03] apt-pkg python-dev
-[04] apt-pkg python3
-[05] apt-pkg php
-[06] apt-pkg java
-[07] apt-pkg git
-[08] apt-pkg perl
-[09] apt-pkg bash
-[10] apt-pkg nano
-[11] apt-pkg curl
-[12] apt-pkg openssl
-[13] apt-pkg openssh
-[14] apt-pkg wget
-[15] apt-pkg clang
-[16] apt-pkg nmap
-[17] apt-pkg w3m
-[18] apt-pkg hydra
-[19] apt-pkg ruby
-[20] apt-pkg macchanger
-[21] apt-pkg host
-[22] apt-pkg dnsutils
-[23] apt-pkg coreutils
-[24] apt-pkg fish
-[25] apt-pkg zip
-[27] apt-pkg tor
-[28] apt-pkg hydra
-[29] apt-pkg figlet 
-[30] apt-pkg cowsay
-[31] apt-pkg tar
-[32] apt-pkg unzip
-[33] apt-pkg vim
-[34] apt-pkg ruby
-[35] apt-pkg wcalc
-[36] apt-pkg bmon
-[37] apt-pkg unrar
-[38] apt-pkg proot
-[39] apt-pkg golang''')
+[1] apt-pkg python
+[2] apt-pkg python2
+[3] apt-pkg php
+[4] apt-pkg bash
+[5] apt-pkg git
+[6] apt-pkg perl
+[7] apt-pkg nano
+[8] apt-pkg curl
+[9] apt-pkg openssl
+[10] apt-pkg openssh
+[11] apt-pkg wget
+[12] apt-pkg clang
+[13] apt-pkg nmap
+[14] apt-pkg w3m
+[15] apt-pkg ruby
+[16] apt-pkg dnsutils
+[17] apt-pkg coreutils
+[18] apt-pkg fish
+[19] apt-pkg zip
+[20] apt-pkg figlet
+[21] apt-pkg cowsay
+[22] apt-pkg unzip
+[23] apt-pkg vim
+[24] apt-pkg wcalc
+[25] apt-pkg bmon
+[26] apt-pkg unrar
+[27] apt-pkg proot
+[28] apt-pkg golang
+[29] apt-pkg tsu
+[30] apt-pkg tor
+                    Что скоро будет                         
+python3
+python-dev
+pip
+pip3
+java
+hydra
+macchanger
+host
+Начинаем Установку''')
 slowprint('''\033[96m
 This Command for access Storage in Termux
 [00] termux-setup-storage''')
@@ -65,115 +67,130 @@ choice = input("\033[93mDo You Want to Install All Packages [y/n] : ")
 if choice == 'n' : sys.exit()
 if choice == 'y' : os.system ("apt update")
 os.system ("apt upgrade -y")
-os.system ("apt install python -y")
-os.system ("apt install python2 -y")
-os.system ("apt install php -y")
-os.system ("apt install python-dev -y")
-os.system ("apt install python3 -y")
-os.system ("apt install java -y")
-os.system ("apt install git -y")
-os.system ("apt install perl -y")
-os.system ("apt install bash")
-os.system ("apt install nano -y")
-os.system ("apt install curl -y")
-os.system ("apt install openssl -y")
-os.system ("apt install openssh -y")
-os.system ("apt install wget -y")
-os.system ("apt install clang -y")
-os.system ("apt install nmap -y")
-os.system ("apt install w3m -y")
-os.system ("apt install hydra -y")
-os.system ("apt install ruby -y")
-os.system ("apt install macchanger -y")
-os.system ("apt install host -y")
-os.system ("apt install dnsutils -y")
-os.system ("apt install coreutils -y")
-os.system ("apt install fish -y")
-os.system ("apt install zip -y")
-os.system ("apt install hydra -y")
-os.system ("apt install figlet -y")
-os.system ("apt install cowsay -y")
-os.system ("apt install unzip -y")
-os.system ("apt install vim -y")
-os.system ("apt install ruby -y")
-os.system ("apt install wcalc -y")
-os.system ("apt install bmon -y")
-os.system ("apt install unrar -y")
-os.system ("apt install proot -y")
-os.system ("apt install golang -y")
-os.system ("pkg upgrade -y")
+os.system ("pkg install")
+os.system ("pkg upgrade")
+os.system ("apt install")
+os.system ("apt upgrate")
+os.system ("apt update")
+os.system ("pkg update")
 os.system ("pkg install python -y")
-os.system ("pkg install python2 -y")
-os.system ("pkg install php -y")
-os.system ("pkg install python-dev -y")
-os.system ("pkg install python3 -y")
-os.system ("pkg install java -y")
-os.system ("pkg install git -y")
-os.system ("pkg install perl -y")
-os.system ("pkg install bash")
-os.system ("pkg install nano -y")
-os.system ("pkg install curl -y")
-os.system ("pkg install openssl -y")
-os.system ("pkg install openssh -y")
-os.system ("pkg install wget -y")
-os.system ("pkg install clang -y")
-os.system ("pkg install nmap -y")
-os.system ("pkg install w3m -y")
-os.system ("pkg install hydra -y")
-os.system ("pkg install ruby -y")
-os.system ("pkg install macchanger -y")
-os.system ("pkg install host -y")
-os.system ("pkg install dnsutils -y")
-os.system ("pkg install coreutils -y")
-os.system ("pkg install fish -y")
-os.system ("pkg install zip -y")
-os.system ("pkg install hydra -y")
-os.system ("pkg install figlet -y")
-os.system ("pkg install cowsay -y")
-os.system ("pkg install unzip -y")
-os.system ("pkg install vim -y")
-os.system ("pkg install ruby -y")
-os.system ("pkg install wcalc -y")
-os.system ("pkg install bmon -y")
-os.system ("pkg install unrar -y")
-os.system ("pkg install proot -y")
-os.system ("pkg install golang -y")
 os.system ("pkg upgrade python -y")
+os.system ("apt install python -y")
+os.system ("apt upgrade python -y")
+os.system ("pkg install python2 -y")
 os.system ("pkg upgrade python2 -y")
+os.system ("apt install python2 -y")
+os.system ("apt upgrade python2 -y")
+os.system ("pkg install php -y")
 os.system ("pkg upgrade php -y")
-os.system ("pkg upgrade python-dev -y")
-os.system ("pkg upgrade python3 -y")
-os.system ("pkg upgrade java -y")
-os.system ("pkg upgrade git -y")
-os.system ("pkg upgrade perl -y")
+os.system ("apt install php -y")
+os.system ("apt upgrade php -y")
+os.system ("pkg install bash")
+os.system ("apt install bash")
 os.system ("pkg upgrade bash")
+os.system ("apt upgrade bash")
+os.system ("apt install git -y")
+os.system ("pkg install git -y")
+os.system ("pkg upgrade git -y")
+os.system ("apt upgrade git -y")
+os.system ("pkg install perl -y")
+os.system ("apt install perl -y")
+os.system ("pkg upgrade perl -y")
+os.system ("apt upgrade perl -y")
+os.system ("pkg install nano -y")
+os.system ("apt install nano -y")
 os.system ("pkg upgrade nano -y")
+os.system ("apt upgrade nano -y")
+os.system ("pkg install curl -y")
+os.system ("apt install curl -y")
 os.system ("pkg upgrade curl -y")
+os.system ("apt upgrade curl -y")
+os.system ("pkg install openssl -y")
+os.system ("apt install openssl -y")
 os.system ("pkg upgrade openssl -y")
+os.system ("apt upgrade openssl -y")
+os.system ("pkg install openssh -y")
+os.system ("apt install openssh -y")
 os.system ("pkg upgrade openssh -y")
+os.system ("apt upgrade openssh -y")
+os.system ("pkg install wget -y")
+os.system ("apt install wget -y")
 os.system ("pkg upgrade wget -y")
+os.system ("apt upgrade wget -y")
+os.system ("pkg install clang -y")
+os.system ("apt install clang -y")
 os.system ("pkg upgrade clang -y")
+os.system ("apt upgrade clang -y")
+os.system ("pkg install nmap -y")
+os.system ("apt install nmap -y")
 os.system ("pkg upgrade nmap -y")
+os.system ("apt upgrade nmap -y")
+os.system ("pkg install w3m -y")
+os.system ("apt install w3m -y")
 os.system ("pkg upgrade w3m -y")
-os.system ("pkg upgrade hydra -y")
+os.system ("apt upgrade w3m -y")
+os.system ("pkg install ruby -y")
+os.system ("apt install ruby -y")
 os.system ("pkg upgrade ruby -y")
-os.system ("pkg upgrade macchanger -y")
-os.system ("pkg upgrade host -y")
+os.system ("apt upgrade ruby -y")
+os.system ("pkg install dnsutils -y")
+os.system ("apt install dnsutils -y")
 os.system ("pkg upgrade dnsutils -y")
+os.system ("apt upgrade dnsutils -y")
+os.system ("pkg install coreutils -y")
+os.system ("apt install coreutils -y")
 os.system ("pkg upgrade coreutils -y")
+os.system ("apt upgrade coreutils -y")
+os.system ("pkg install fish -y")
+os.system ("apt install fish -y")
 os.system ("pkg upgrade fish -y")
+os.system ("apt upgrade fish -y")
+os.system ("pkg install zip -y")
+os.system ("apt install zip -y")
 os.system ("pkg upgrade zip -y")
-os.system ("pkg upgrade hydra -y")
+os.system ("apt upgrade zip -y")
+os.system ("pkg install figlet -y")
+os.system ("apt install figlet -y")
 os.system ("pkg upgrade figlet -y")
+os.system ("apt upgrade figlet -y")
+os.system ("pkg install cowsay -y")
+os.system ("apt install cowsay -y")
 os.system ("pkg upgrade cowsay -y")
+os.system ("apt upgrade cowsay -y")
+os.system ("pkg install unzip -y")
+os.system ("apt install unzip -y")
 os.system ("pkg upgrade unzip -y")
+os.system ("apt upgrade unzip -y")
+os.system ("pkg install vim -y")
+os.system ("apt install vim -y")
 os.system ("pkg upgrade vim -y")
-os.system ("pkg upgrade ruby -y")
+os.system ("apt upgrade vim -y")В р
+os.system ("pkg install wcalc -y")
+os.system ("apt install wcalc -y")
 os.system ("pkg upgrade wcalc -y")
+os.system ("apt upgrade wcalc -y")
+os.system ("pkg install bmon -y")
+os.system ("apt install bmon -y")
 os.system ("pkg upgrade bmon -y")
+os.system ("apt upgrade bmon -y")
+os.system ("pkg install unrar -y")
+os.system ("apt install unrar -y")
 os.system ("pkg upgrade unrar -y")
+os.system ("apt upgrade unrar -y")
+os.system ("pkg install proot -y")
+os.system ("apt install proot -y")
 os.system ("pkg upgrade proot -y")
+os.system ("apt upgrade proot -y")
+os.system ("pkg install golang -y")
+os.system ("apt install golang -y")
 os.system ("pkg upgrade golang -y")
+os.system ("apt upgrade golang -y")
+os.system("pkg install tsu-y")
+os.system ("apt install tsu -y")
+os.system ("pkg upgrade tsu -y")
+os.system ("apt upgrade tsu -y")
+os.system ("pkg install tor")
+
 
 print ("Allow the Button For Access the Storage in Termux")
 
